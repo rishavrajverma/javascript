41360db3586a078f5403769109d8de0f0d6fdf9c
rishab raj verma <mfsi.Rishabrajv@outlook.com>
2025-07-17 10:41:06 +0530

    style : removed bootstrap class

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-//practice protype in object
+//practice prototype in object
 
 //console.log(jobType.__proto__);
 // console.log(jobType.constructor);
@@ -9,13 +9,39 @@ box.prototype.getValue = function () {
   return this.value;
 };
 box.prototype.name = "rishab";
+
 const b1 = new box(4);
 
 // console.log(b1.getValue());
 // console.log(b1);
 
-console.log(
-  (() => {
-    return "rishab";
-  })()
-);
+// console.log(
+//   (() => {
+//     return "rishab";
+//   })()
+// );
+
+//console.log(box.prototype);
+
+const obj = {
+  name: "rishab",
+  myFunction: () => {
+    name = "rishab raj verma";
+    return name;
+  },
+};
+
+const o1 = Object.create(obj);
+
+//console.log(o1.__proto__);
+
+function fun() {
+  const name = "ronny";
+}
+
+const f1 = new fun();
+fun.prototype.newFun = () => {
+  console.log("hi");
+};
+
+//f1.newFun();
